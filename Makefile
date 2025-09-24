61ebe893965a38fd579f1161e6c893d527beb480
krnl15 <krnl@hatofuji.me>
2025-09-25 04:41:00 +0900

    changed from minilibx (too old) to mlx42

--- Makefile
+++ Makefile
@@ -4,37 +4,20 @@ NAME = fdf
 BUILD_DIR = build/
 INCLUDE_DIR = include/
 SOURCE_DIR = src/
-MLX_DIR = lib/minilibx-linux
 
-UNAME_S := $(shell uname -s)
-
-ifeq ($(UNAME_S),Darwin)
-    MLX_FLAGS = -L$(MLX_DIR) -lmlx -framework OpenGL -framework AppKit
-    MLX_INCLUDE = -I$(MLX_DIR)
-    EXTRA_FLAGS = 
-else ifeq ($(UNAME_S),Linux)
-    MLX_FLAGS = -L$(MLX_DIR) -lmlx -lXext -lX11 -lm -lz
-    MLX_INCLUDE = -I$(MLX_DIR)
-    EXTRA_FLAGS = 
-endif
+MLX_FLAGS = -lmlx42 -lglfw -pthread -lm -ldl
 
 all: clean build
 
 clean:
 	rm -rf $(BUILD_DIR)$(NAME)
 
-$(MLX_DIR)/libmlx.a:
-	make -C $(MLX_DIR)
-
-build: $(MLX_DIR)/libmlx.a $(INCLUDE_DIR)* $(SOURCE_DIR)$(NAME).c
+build: $(INCLUDE_DIR)* $(SOURCE_DIR)$(NAME).c
 	mkdir -p $(BUILD_DIR)
-	sudo cp $(INCLUDE_DIR)$(NAME).h /usr/local/include
 	gcc $(FLAGS) -g $(MLX_INCLUDE) -o $(BUILD_DIR)$(NAME) $(SOURCE_DIR)$(NAME).c $(MLX_FLAGS)
+	
+include:
+	sudo cp $(INCLUDE_DIR)$(NAME).h /usr/local/include
 
-debug:
-	@echo "OS détecté: $(UNAME_S)"
-	@echo "Flags MLX: $(MLX_FLAGS)"
-	@echo "Include MLX: $(MLX_INCLUDE)"
 
 .PHONY: all clean build debug
-
--- src/fdf.c
+++ src/fdf.c
@@ -1,57 +1,34 @@
+#include <MLX42/MLX42.h>
 #include <fdf.h>
 #include <libft/libft.h>
-#include <mlx.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 800
 
-typedef struct vars_s {
-  void *win;
-  void *mlx;
-} vars_t;
+void ft_hook(void *p) {
+  const mlx_t *mlx = p;
 
-typedef struct data_s {
-  void *img;
-  char *addr;
-  i32 bits_per_pixel;
-  i32 line_length;
-  i32 endian;
-} data_t;
-
-u32 get_offset(data_t *img, u32 x, u32 y) {
-  return (y * img->line_length + x * (img->bits_per_pixel / 8));
-}
-
-void put_pixel(data_t *buf, u32 x, u32 y, u32 color) {
-  char *dst;
-
-  if (x >= WIN_WIDTH || y >= WIN_HEIGHT)
-    return;
-
-  dst = buf->addr + get_offset(buf, x, y);
-  *(unsigned int *)dst = color;
+  printf("w: %d | h: %d\n", mlx->width, mlx->height);
 }
 
 int main(void) {
-  vars_t vars;
-  vars.mlx = mlx_init();
-  vars.win = mlx_new_window(vars.mlx, WIN_WIDTH, WIN_HEIGHT, "fdf");
+  mlx_t *mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "fdf", 0);
+  if (!mlx)
+    return 1;
 
-  data_t img;
-  img.img = mlx_new_image(vars.mlx, WIN_WIDTH, WIN_HEIGHT);
+  mlx_image_t *img = mlx_new_image(mlx, WIN_WIDTH, WIN_HEIGHT);
+  if (!img || (mlx_image_to_window(mlx, img, 0, 0)) < 0)
+    return 1;
 
-  img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-                               &img.endian);
-
-  for (u32 i = 100; i < 500; ++i) {
-    for (u32 j = 100; j < 500; ++j) {
-      put_pixel(&img, i, j, 0x00FF0000);
+  for (u32 i = 100; i < 300; ++i) {
+    for (u32 j = 100; j < 300; ++j) {
+      mlx_put_pixel(img, i, j, 0xFF0000AA);
     }
   }
-  mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-  mlx_loop(vars.mlx);
-
-  mlx_destroy_window(vars.mlx, vars.win);
+  mlx_loop_hook(mlx, ft_hook, mlx);
+  mlx_loop(mlx);
+  mlx_terminate(mlx);
   return 0;
 }
