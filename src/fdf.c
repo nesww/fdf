7f6968d57fa245ab0100b2a52dcec43e33890242
lcollomb <loancollomb@icloud.com>
2025-09-26 14:25:25 +0200

    started drawing buf

--- src/fdf.c
+++ src/fdf.c
@@ -5,12 +5,14 @@
 #include <libft/ftio/ftio.h>
 #include <libft/ftypes.h>
 #include <libft/libft.h>
+#include <limits.h>
 #include <math.h>
 #include <stdlib.h> //for free/malloc
 #include <unistd.h>
 
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 600
+#define DIST_SCALE 50 // px
 
 #define RED 0xff0000ff
 #define BLUE 0x0000ffff
@@ -74,8 +76,8 @@ void draw_line(mlx_image_t *img, vec2 *src, vec2 *dst, u32 color) {
   }
 }
 
-void free_buf(i32 **buf, i32 len) {
-  for (u32 j = 0; j < (u32)len; ++j) {
+void free_buf(i32 **buf, u32 len) {
+  for (u32 j = 0; j < len; ++j) {
     if (buf[j])
       free(buf[j]);
   }
@@ -123,11 +125,12 @@ i32 **load_fdf(int fd, char *fdf_path) {
     while (split[word_count])
       ++word_count;
 
-    buf[i] = malloc(sizeof(i32) * word_count);
+    buf[i] = malloc(sizeof(i32) * (word_count) + 1);
     for (u32 j = 0; j < word_count; ++j) {
       buf[i][j] = ft_atoi(split[j]);
       free(split[j]);
     }
+    buf[i][word_count] = INT_MAX;
     free(split);
     free(line);
 
@@ -138,6 +141,28 @@ i32 **load_fdf(int fd, char *fdf_path) {
   return buf;
 }
 
+u32 buf_len(i32 **buf) {
+  u32 k = 0;
+  while (buf[k])
+    ++k;
+  return k;
+}
+
+void draw_buf(mlx_image_t *img, i32 **buf, u32 offset) {
+  u32 len = buf_len(buf);
+  io_printf("len= %d\n", len);
+  for (u32 i = 0; i < len; ++i) {
+    u32 j = 0;
+    while (buf[i][j]) {
+      io_printf("drawing x:%d y:%d, given offset=%d (x:%d y: %d)\n", i, j,
+                offset, i + i * offset, j + j * offset);
+      vec2 pos = {i + i * offset, j + j * offset};
+      draw_circle(img, &pos, 3, 0xff0000aa);
+      ++j;
+    }
+    io_printf("j=%d after one loop (of i=%d)\n", j, i);
+  }
+}
 int main(int argc, char **argv) {
   io_printf("Startup of fdf...\n");
   // load  of file from args
@@ -160,10 +185,6 @@ int main(int argc, char **argv) {
   if (!buf) {
     io_printf("fatal: failed to load `%s` into memory, exiting\n");
   }
-  i32 buf_len = 0;
-  while (buf[buf_len])
-    buf_len++;
-  free_buf(buf, buf_len);
   io_printf("closing fd\n", fdf_path);
   close(fdf_file);
 
@@ -182,24 +203,26 @@ int main(int argc, char **argv) {
   if (!img || (mlx_image_to_window(mlx, img, 0, 0)) < 0)
     return 1;
 
-  vec2 src = {.x = WIN_WIDTH / 2, .y = WIN_HEIGHT / 2};
+  // vec2 src = {.x = WIN_WIDTH / 2, .y = WIN_HEIGHT / 2};
 
-  vec2 left = {src.x - 100, src.y};
-  vec2 right = {src.x + 100, src.y};
+  // vec2 left = {src.x - 100, src.y};
+  // vec2 right = {src.x + 100, src.y};
 
-  vec2 rotated_left = rotate_line(&src, &left, 30);
-  vec2 rotated_right = rotate_line(&src, &right, -30);
+  // vec2 rotated_left = rotate_line(&src, &left, 30);
+  // vec2 rotated_right = rotate_line(&src, &right, -30);
 
-  clear_image(img, BLACK);
+  // clear_image(img, BLACK);
 
-  draw_line(img, &src, &left, BLUE);
-  draw_line(img, &src, &right, GREEN);
-  draw_line(img, &src, &rotated_left, GREEN);
-  draw_line(img, &src, &rotated_right, BLUE);
-  draw_line(img, &rotated_left, &rotated_right, 0xff000077);
-  draw_circle(img, &src, 5, RED);
+  // draw_line(img, &src, &left, BLUE);
+  // draw_line(img, &src, &right, GREEN);
+  // draw_line(img, &src, &rotated_left, GREEN);
+  // draw_line(img, &src, &rotated_right, BLUE);
+  // draw_line(img, &rotated_left, &rotated_right, 0xff000077);
+  // draw_circle(img, &src, 5, RED);
+  draw_buf(img, buf, DIST_SCALE);
   io_printf("starting mlx loop\n");
   mlx_loop(mlx);
+  free_buf(buf, buf_len(buf));
   io_printf("closing...\n");
   mlx_terminate(mlx);
   return 0;
