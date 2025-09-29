47d6b8d38c21f4e85325a58b0dc3b601a9fdf6a7
nes <krnl@hatofuji.me>
2025-09-30 05:49:30 +0900

    first version for basic fdf files, need checks for big fdf

--- src/fdf.c
+++ src/fdf.c
@@ -10,9 +10,10 @@
 #include <stdlib.h> //for free/malloc
 #include <unistd.h>
 
-#define WIN_WIDTH 800
-#define WIN_HEIGHT 600
-#define DIST_SCALE 50 // px
+#define WIN_WIDTH 1280
+#define WIN_HEIGHT 1080
+#define DIST_SCALE 5 // px
+#define COLOR_SCALE 20
 
 #define RED 0xff0000ff
 #define BLUE 0x0000ffff
@@ -25,6 +26,28 @@ typedef struct vec2_s {
   i32 y;
 } vec2;
 
+typedef struct color_s {
+  u8 r;
+  u8 g;
+  u8 b;
+  u8 a;
+} Color;
+
+typedef struct fdfmap_s {
+  i32 **buf;
+  u32 len;
+  u32 width;
+} fdfmap_t;
+
+u32 color_to_hex(Color *c) {
+  u32 hex = 0x0;
+  hex = (hex << 8) + c->r;
+  hex = (hex << 8) + c->g;
+  hex = (hex << 8) + c->b;
+  hex = (hex << 8) + c->a;
+  return hex;
+}
+
 u32 ft_abs(i32 n) { return n < 0 ? -n : n; }
 
 u32 euclidian_dist_sq(vec2 *src, vec2 *dst) {
@@ -33,13 +56,25 @@ u32 euclidian_dist_sq(vec2 *src, vec2 *dst) {
   return ft_abs(dx * dx + dy * dy);
 }
 
-void draw_circle(mlx_image_t *img, vec2 *center_pos, u32 radius, u32 color) {
+void draw_circle(mlx_image_t *img, vec2 *center_pos, u32 radius, Color *color) {
+  u32 c = color_to_hex(color);
   for (u32 x = center_pos->x - radius; x < center_pos->x + radius; ++x) {
     for (u32 y = center_pos->y - radius; y < center_pos->y + radius; ++y) {
       vec2 p = {.x = x, .y = y};
       if (euclidian_dist_sq(center_pos, &p) < radius * radius) {
-        mlx_put_pixel(img, x, y, color);
+        mlx_put_pixel(img, x, y, c);
       }
+    }
+  }
+}
+
+void draw_square(mlx_image_t *img, vec2 *center_pos, u32 radius, Color *color) {
+  // io_printf("will draw at x:%d y:%d with a radius of %d\n", center_pos->x,
+  //          center_pos->y, radius);
+  u32 c = color_to_hex(color);
+  for (u32 x = center_pos->x - radius; x < center_pos->x + radius; ++x) {
+    for (u32 y = center_pos->y - radius; y < center_pos->y + radius; ++y) {
+      mlx_put_pixel(img, x, y, c);
     }
   }
 }
@@ -104,7 +139,14 @@ void clear_image(mlx_image_t *img, u32 color) {
   }
 }
 
-i32 **load_fdf(int fd, char *fdf_path) {
+u32 buf_len(i32 **buf) {
+  u32 k = 0;
+  while (buf[k])
+    ++k;
+  return k;
+}
+
+fdfmap_t *load_fdf(int fd, char *fdf_path) {
   i32 **buf = malloc(sizeof(i32 *) * 4096);
   if (!buf) {
     io_printf("error: could not malloc for buf of fdf\n");
@@ -117,10 +159,11 @@ i32 **load_fdf(int fd, char *fdf_path) {
   }
 
   u32 i = 0;
+  u32 word_count;
   io_printf("loading fdf from `%s` into memory...", fdf_path);
   while (line) {
 
-    u32 word_count = 0;
+    word_count = 0;
     char **split = ft_split(line, ' ');
     while (split[word_count])
       ++word_count;
@@ -137,32 +180,221 @@ i32 **load_fdf(int fd, char *fdf_path) {
     line = get_next_line(fd);
     ++i;
   }
+  fdfmap_t *fdf = malloc(sizeof(fdfmap_t));
+  fdf->buf = buf;
+  fdf->len = buf_len(fdf->buf);
+  fdf->width = word_count - 1;
+
   io_printf("success\n");
-  return buf;
+  return fdf;
 }
 
-u32 buf_len(i32 **buf) {
-  u32 k = 0;
-  while (buf[k])
-    ++k;
-  return k;
+u8 normalize_hex(u32 hex) {
+  if (hex > 0xff) {
+    return 0xff;
+  }
+  return hex;
 }
 
-void draw_buf(mlx_image_t *img, i32 **buf, u32 offset) {
-  u32 len = buf_len(buf);
-  io_printf("len= %d\n", len);
-  for (u32 i = 0; i < len; ++i) {
+void draw_buf(mlx_image_t *img, fdfmap_t *fdf, u32 offset) {
+
+  io_printf("draw_buf called with offset= %d\n", offset);
+  io_printf("fdf->len: %d\n", fdf->len);
+  io_printf("fdf->width: %d\n", fdf->width);
+
+  vec2 start = {(WIN_WIDTH - (fdf->width * offset)) / 2,
+                (WIN_HEIGHT - (fdf->len * offset)) / 2};
+
+  if (start.x < 0 || start.x > WIN_WIDTH) {
+    start.x = 0;
+    offset = 2;
+  }
+  if (start.y < 0 || start.y > WIN_WIDTH) {
+    start.y = 0;
+    offset = 2;
+  }
+
+  io_printf("start(%d, %d)\n", start.x, start.y);
+
+  for (u32 i = 0; i < fdf->len; ++i) {
     u32 j = 0;
-    while (buf[i][j]) {
-      io_printf("drawing x:%d y:%d, given offset=%d (x:%d y: %d)\n", i, j,
-                offset, i + i * offset, j + j * offset);
-      vec2 pos = {i + i * offset, j + j * offset};
-      draw_circle(img, &pos, 3, 0xff0000aa);
+    while (fdf->buf[i][j] != INT_MAX) {
+      vec2 pos = {start.x + j * offset, start.y + i * offset};
+      // io_printf("pos(%d,%d)\n", pos.x, pos.y);
+      if (pos.x < 0 || pos.x >= (i32)WIN_WIDTH || pos.y < 0 ||
+          pos.y >= (i32)WIN_HEIGHT) {
+        // io_printf("warning: computed pos(%d,%d), out of image, skipping\n",
+        //          pos.x, pos.y);
+        ++j;
+        continue;
+      }
+
+      // if (i % 5 && j % 5) {
+      Color c = {normalize_hex(0x66 + fdf->buf[i][j] * COLOR_SCALE),
+                 normalize_hex(0x88 + fdf->buf[i][j] * COLOR_SCALE),
+                 normalize_hex(0x33 + fdf->buf[i][j] * COLOR_SCALE), 0xff};
+
+      // io_printf("Color: (%x, %x, %x, %x)\n", c.r, c.g, c.b, c.a);
+      draw_square(img, &pos, offset / 1.8, &c);
+      //}
       ++j;
     }
-    io_printf("j=%d after one loop (of i=%d)\n", j, i);
   }
 }
+
+typedef struct iso_point_s {
+  vec2 *pos;
+  vec2 *east_conn;
+  vec2 *south_conn;
+} iso_point_t;
+
+// iso_point_t rec_iso_point_neighbors_of(vec2 *pos, fdfmap_t *fdf) {
+
+// stop condition -> found bottom_right corner has no south AND no east
+//  if (pos->x + 1 >= fdf->width && pos->y + 1 >= fdf->len) {
+//    return (iso_point_t){pos, NULL, NULL};
+//  }
+
+// right border, no east, but south
+// if (pos->x + 1 >= fdf->width && !(pos->y + 1 >= fdf->len)) {
+//  vec2 tmp = {pos->x, pos->y + 1};
+// iso_point_t south = rec_iso_point_neighbors_of(&tmp, fdf);
+// return (iso_point_t){pos, NULL, south.pos};
+// }
+// bottom border, no south but east
+// else if (pos->y >= fdf->len) {
+// vec2 tmp = {pos->x + 1, pos->y};
+//  iso_point_t east = rec_iso_point_neighbors_of(&tmp, fdf);
+//  return (iso_point_t){pos, east.pos, NULL};
+// }
+// has both east and south
+// else {
+// vec2 tmp_east = {pos->x + 1, pos->y};
+// vec2 tmp_south = {pos->x, pos->y + 1};
+// iso_point_t east = rec_iso_point_neighbors_of(&tmp_east, fdf);
+// iso_point_t south = rec_iso_point_neighbors_of(&tmp_south, fdf);
+// return (iso_point_t){pos, east.pos, south.pos};
+//}
+//}
+
+void clean_iso_points(iso_point_t *buf, u32 cursor) {
+  for (u32 i = 0; i < cursor; ++i) {
+    free(buf[i].pos);
+    free(buf[i].east_conn);
+    free(buf[i].south_conn);
+  }
+  free(buf);
+}
+
+// get all isometrics positions for all points of ftmap_t
+iso_point_t *iso_points(fdfmap_t *fdf, u32 offset_x, u32 offset_y, vec2 *origin,
+                        i32 height_scale) {
+
+  iso_point_t *points = malloc(sizeof(iso_point_t) * (fdf->len * fdf->width));
+  if (!points)
+    return NULL;
+  u32 points_cursor = 0;
+  for (u32 y = 0; y < fdf->len; ++y) {
+    for (u32 x = 0; x < fdf->width; ++x) {
+
+      vec2 *pos = malloc(sizeof(vec2));
+      if (!pos) {
+        clean_iso_points(points, points_cursor);
+        return NULL;
+      }
+      pos->x = (x - y) * offset_x + origin->x;
+      pos->y = (x + y) * offset_y - fdf->buf[y][x] * height_scale + origin->y;
+
+      vec2 *east_conn;
+      if (x + 1 >= fdf->width) {
+        east_conn = NULL;
+      } else {
+        east_conn = malloc(sizeof(vec2));
+        if (!east_conn) {
+          clean_iso_points(points, points_cursor);
+          free(points);
+          return NULL;
+        }
+        east_conn->x = ((x + 1) - y) * offset_x + origin->x;
+        east_conn->y = ((x + 1) + y) * offset_y -
+                       fdf->buf[y][x + 1] * height_scale + origin->y;
+      }
+
+      vec2 *south_conn;
+      if (y + 1 >= fdf->len) {
+        south_conn = NULL;
+      } else {
+        south_conn = malloc(sizeof(vec2));
+        if (!south_conn) {
+          clean_iso_points(points, points_cursor);
+          return NULL;
+        }
+        south_conn->x = (x - (y + 1)) * offset_x + origin->x;
+        south_conn->y = (x + (y + 1)) * offset_y -
+                        fdf->buf[y + 1][x] * height_scale + origin->y;
+      }
+
+      iso_point_t p = {
+          .pos = pos, .east_conn = east_conn, .south_conn = south_conn};
+
+      // io_printf("initial pos: pos(%d,%d,%d) of fdf\n", x, y, fdf->buf[y][x]);
+      points[points_cursor] = p;
+      ++points_cursor;
+    }
+  }
+  return points;
+}
+
+typedef struct s_vars {
+  mlx_t *mlx;
+  mlx_image_t *img;
+  fdfmap_t *fdf;
+  int height_scale;
+  u32 offset_x;
+  u32 offset_y;
+  vec2 *origin;
+} vars_t;
+
+void redraw(vars_t *vars) {
+  iso_point_t *points = iso_points(vars->fdf, vars->offset_x, vars->offset_y,
+                                   vars->origin, vars->height_scale);
+  clear_image(vars->img, 0x3333333f);
+
+  io_printf("redrawing!\n");
+
+  for (u32 i = 0; i < vars->fdf->width * vars->fdf->len; ++i) {
+    if (points[i].pos->x < 0 || points[i].pos->y < 0 ||
+        points[i].pos->x >= WIN_WIDTH || points[i].pos->y >= WIN_HEIGHT) {
+      io_printf("skipping point of pos(%d,%d) -> out of bound\n",
+                points[i].pos->x, points[i].pos->y);
+      continue;
+    }
+    Color red = {0xff, 0x00, 0x00, 0xff};
+    draw_circle(vars->img, points[i].pos, 2, &red);
+    if (points[i].east_conn)
+      draw_line(vars->img, points[i].pos, points[i].east_conn, 0xffffffff);
+    if (points[i].south_conn)
+      draw_line(vars->img, points[i].pos, points[i].south_conn, 0xffffffff);
+  }
+}
+
+void key_handler(mlx_key_data_t keydata, void *param) {
+  vars_t *vars = (vars_t *)param;
+  if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS) {
+    io_printf("key_up event!\n");
+    if (vars->height_scale + 1 < 30)
+      vars->height_scale += 1;
+    redraw(vars);
+  } else if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS) {
+    io_printf("key_down event!\n");
+    if (vars->height_scale - 1 > -10)
+      vars->height_scale -= 1;
+    redraw(vars);
+  } else if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS) {
+    mlx_close_window(vars->mlx);
+  }
+}
+
 int main(int argc, char **argv) {
   io_printf("Startup of fdf...\n");
   // load  of file from args
@@ -181,8 +413,8 @@ int main(int argc, char **argv) {
   }
 
   // load fdf into a buffer
-  i32 **buf = load_fdf(fdf_file, fdf_path);
-  if (!buf) {
+  fdfmap_t *fdf = load_fdf(fdf_file, fdf_path);
+  if (!fdf) {
     io_printf("fatal: failed to load `%s` into memory, exiting\n");
   }
   io_printf("closing fd\n", fdf_path);
@@ -203,26 +435,45 @@ int main(int argc, char **argv) {
   if (!img || (mlx_image_to_window(mlx, img, 0, 0)) < 0)
     return 1;
 
-  // vec2 src = {.x = WIN_WIDTH / 2, .y = WIN_HEIGHT / 2};
+  // offset for scaling in the window
+  i32 h_offset = WIN_WIDTH / fdf->width;
+  i32 v_offset = WIN_HEIGHT / fdf->len;
+  i32 offset = h_offset > v_offset ? v_offset : h_offset;
+  if (offset < 5)
+    offset = 5;
 
-  // vec2 left = {src.x - 100, src.y};
-  // vec2 right = {src.x + 100, src.y};
+  // draw_buf(img, fdf, offset);
+  vec2 iso_center = {WIN_WIDTH / 2, WIN_HEIGHT / 3};
+  i32 height_scale = 1;
+  iso_point_t *points =
+      iso_points(fdf, h_offset / 4, v_offset / 4, &iso_center, height_scale);
 
-  // vec2 rotated_left = rotate_line(&src, &left, 30);
-  // vec2 rotated_right = rotate_line(&src, &right, -30);
+  // draw points
+  for (u32 i = 0; i < fdf->width * fdf->len; ++i) {
+    if (points[i].pos->x < 0 || points[i].pos->y < 0 ||
+        points[i].pos->x >= WIN_WIDTH || points[i].pos->y >= WIN_HEIGHT) {
+      io_printf("skipping point of pos(%d,%d) -> out of bound\n",
+                points[i].pos->x, points[i].pos->y);
+      continue;
+    }
+    Color red = {0xff, 0x00, 0x00, 0xff};
+    draw_circle(img, points[i].pos, 2, &red);
+    if (points[i].east_conn)
+      draw_line(img, points[i].pos, points[i].east_conn, 0xffffffff);
+    if (points[i].south_conn)
+      draw_line(img, points[i].pos, points[i].south_conn, 0xffffffff);
+  }
+  free(points);
 
-  // clear_image(img, BLACK);
-
-  // draw_line(img, &src, &left, BLUE);
-  // draw_line(img, &src, &right, GREEN);
-  // draw_line(img, &src, &rotated_left, GREEN);
-  // draw_line(img, &src, &rotated_right, BLUE);
-  // draw_line(img, &rotated_left, &rotated_right, 0xff000077);
-  // draw_circle(img, &src, 5, RED);
-  draw_buf(img, buf, DIST_SCALE);
   io_printf("starting mlx loop\n");
+  vars_t vars = {mlx,          img,          fdf,        height_scale,
+                 h_offset / 4, v_offset / 4, &iso_center};
+
+  mlx_key_hook(mlx, key_handler, (void *)&vars);
+
   mlx_loop(mlx);
-  free_buf(buf, buf_len(buf));
+
+  free_buf(fdf->buf, fdf->len);
   io_printf("closing...\n");
   mlx_terminate(mlx);
   return 0;
